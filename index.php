d610259acb930159d9615d92b8d7f12b8283754b
primemix <monstrpro@gmail.com>
2017-10-12 20:27:59 +0300

    fix conflicts

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
+
 echo 'Master branch';
+echo '1.0 branch ->feature_bar';
+//this is first changed in feature_bar
+//this is changed in feature_foo_1.0
+//this is more changed in feature_foo_1.0
+
